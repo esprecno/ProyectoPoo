82c8f30ec85dede4c5e3600f1fe678ed6ef8abfc
Fabio Alejandro Camargo Díaz <fcamargodiaz@gmail.com>
2019-03-10 15:04:41 -0500

    Se agrega el menú en el main
    No tiene funcionalidad aún, no es más que el switch

--- Agencia_de_turismo/src/Boundarys/PantallaAgencia.java
+++ Agencia_de_turismo/src/Boundarys/PantallaAgencia.java
@@ -5,10 +5,79 @@
  */
 package Boundarys;
 
+import java.util.Scanner;
+
 /**
  *
  * @author alejo
  */
 public class PantallaAgencia {
-    
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Escoja la opcion deseada:");
+        System.out.println("1)Ver listado de tours disponibles");
+        System.out.println("2)Insertar Tour");
+        System.out.println("3)Modificar Tour");
+        System.out.println("4)Eliminar Tour");
+        System.out.println("5)Ver listado de clientes en el sistema");
+        System.out.println("6)Insertar cliente");
+        System.out.println("7)Modificar datos del cliente");
+        System.out.println("8)Eliminar un cliente");
+        System.out.println("9)Reservar Tour");
+        System.out.println("10)Modificar reserva Tour");
+        System.out.println("11)Eliminar reserva Tour");
+        System.out.println("12)Ver listado de reservas existentes");
+        System.out.println("13)Ver listado de reservas para un tour fecha especifica");
+        System.out.println("14)SALIR");
+        int x = sc.nextInt();
+        switch (x) {
+            case 1: {
+
+                break;
+            }
+            case 2: {
+                break;
+            }
+            case 3: {
+                break;
+            }
+            case 4: {
+                break;
+            }
+            case 5: {
+                break;
+            }
+            case 6: {
+                break;
+            }
+            case 7: {
+                break;
+            }
+            case 8: {
+                break;
+            }
+            case 9: {
+                break;
+            }
+            case 10: {
+                break;
+            }
+            case 11: {
+                break;
+            }
+            case 12: {
+                break;
+            }
+            case 13: {
+                break;
+            }
+            case 14: {
+                break;
+            }
+        }
+
+    }
+
 }
